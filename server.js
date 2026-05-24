07a1c8c3f41dce311cd142e2c0f486b24988cb46
Abdullahi Ramadan <ramadanayinla93@gmail.com>
2026-05-24 22:04:27 +0100

    Implement GET endpoint for fetching students
    Added endpoint to retrieve all students from Firestore.

--- server.js
+++ server.js
@@ -19,7 +19,33 @@ app.post("/register", async (req, res) => {
 
         // Save to Firebase Firestore
         await db.collection("students").add(data);
+// GET ALL STUDENTS
+app.get("/students", async (req, res) => {
 
+    try {
+
+        const snapshot = await db.collection("students").get();
+
+        let students = [];
+
+        snapshot.forEach(doc => {
+            students.push({
+                id: doc.id,
+                ...doc.data()
+            });
+        });
+
+        res.json(students);
+
+    } catch (error) {
+
+        res.status(500).json({
+            error: error.message
+        });
+
+    }
+
+});
         // Send email
    const transporter = nodemailer.createTransport({
     host: "smtp-relay.brevo.com",

db7cff5857010f56f759b902e842a74707b9db3c
Abdullahi Ramadan <ramadanayinla93@gmail.com>
2026-06-29 00:04:30 +0100

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const ADMIN_PIN = process.env.ADMIN_PIN;
 const db = require("./firebase");
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
